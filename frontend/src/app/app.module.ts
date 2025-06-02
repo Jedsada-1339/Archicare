592e0f62cf0f88854a17e06a2f89757dd411d3dc
Jedsada-1339 <jedsada.si@kkumail.com>
2025-06-02 21:52:10 +0700

    learn how to do path

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HomeComponent,
+    BlogComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
