c6a7c7629143ca7700df168e43fe3fbbd1467213
Jedsada-1339 <jedsada.si@kkumail.com>
2025-06-08 16:22:34 +0700

    update reusable item

--- frontend/src/app/Reusable/rate/rate.component.ts
+++ frontend/src/app/Reusable/rate/rate.component.ts
@@ -1,3 +1,4 @@
+// rate.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -16,45 +17,51 @@ export class RateComponent {
 
   toggleLike() {
     if (this.liked) {
+      // Cancel like - no notification
       this.likes--;
+      this.liked = false;
     } else {
+      // Activate like - show notification
       this.likes++;
       if (this.disliked) {
         this.disliked = false;
         this.dislikes--;
       }
+      this.liked = true;
+      
+      // Show like notification only when activating
+      this.showLikeNotification = true;
+      this.showDislikeNotification = false;
+      
+      // Hide notification after 3 seconds
+      setTimeout(() => {
+        this.showLikeNotification = false;
+      }, 500);
     }
-    this.liked = !this.liked;
-    
-    // Show like notification
-    this.showLikeNotification = true;
-    this.showDislikeNotification = false;
-    
-    // Hide notification after 3 seconds
-    setTimeout(() => {
-      this.showLikeNotification = false;
-    }, 1500);
   }
 
   toggleDislike() {
     if (this.disliked) {
+      // Cancel dislike - no notification
       this.dislikes--;
+      this.disliked = false;
     } else {
+      // Activate dislike - show notification
       this.dislikes++;
       if (this.liked) {
         this.liked = false;
         this.likes--;
       }
+      this.disliked = true;
+      
+      // Show dislike notification only when activating
+      this.showDislikeNotification = true;
+      this.showLikeNotification = false;
+      
+      // Hide notification after 3 seconds
+      setTimeout(() => {
+        this.showDislikeNotification = false;
+      }, 500);
     }
-    this.disliked = !this.disliked;
-    
-    // Show dislike notification
-    this.showDislikeNotification = true;
-    this.showLikeNotification = false;
-    
-    // Hide notification after 3 seconds
-    setTimeout(() => {
-      this.showDislikeNotification = false;
-    }, 1500);
   }
 }
