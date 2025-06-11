75aa3d1b89cb5a3f17ea491d927d7fcf040b54fc
Jedsada-1339 <jedsada.si@kkumail.com>
2025-06-12 01:27:41 +0700

    update Post-page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,7 +42,8 @@ import { PostPageComponent } from './post-page/post-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/post-page/post-page.component.ts
+++ frontend/src/app/post-page/post-page.component.ts
@@ -15,7 +15,7 @@ export class PostPageComponent {
 
   roomOptions = ['Living Room', 'Kitchen', 'Balcony / Storage Area', 'Bathroom'];
 
-  allCategory = ['One-storyhouse','Two-story house','Apartment','Townhouse'];
+  allCategory = ['One-storyhouse', 'Two-story house', 'Apartment', 'Townhouse'];
 
   togglePostHouse() {
     this.onSelectedHouse = "text-indigo-600 shadow bg-white"
@@ -33,5 +33,42 @@ export class PostPageComponent {
     this.onFormHouse = false
     this.onFormBlog = true
   }
+
+  // MultipleFilesSelected
+  imagePreviews: string[] = [];
+
+  onMultipleFilesSelected(event: Event): void {
+    const files = (event.target as HTMLInputElement).files;
+    if (!files || files.length === 0) return;
+
+    Array.from(files).forEach(file => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.result) {
+          this.imagePreviews.push(reader.result.toString());
+        }
+      };
+      reader.readAsDataURL(file);
+    });
+  }
+
+  removeImage(index: number): void {
+    this.imagePreviews.splice(index, 1);
+  }
+
+  // SingleFilesSelected
+  singleImagePreview: string | ArrayBuffer | null = null;
+
+  onSingleFileSelected(event: Event): void {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.singleImagePreview = reader.result;
+    };
+    reader.readAsDataURL(file); // Convert to base64
+  }
+
 
 }
