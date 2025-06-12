cef06ec2b15b154919ccfdd30c671ec5f79845b3
Jedsada-1339 <jedsada.si@kkumail.com>
2025-06-12 14:36:03 +0700

    fix card TS

--- frontend/src/app/Reusable/card/card.component.ts
+++ frontend/src/app/Reusable/card/card.component.ts
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-
+  houseDetails = {
+    title: 'Two-storey modern house',
+    area: {
+      total: 137,
+      usable: 93,
+      terrace: 6,
+      garden: 34
+    },
+    rooms: {
+      bedrooms: 3,
+      bathrooms: 2,
+      livingRoom: true,
+      kitchen: true,
+      terrace: true
+    },
+    tag: {
+      onestoryhouse: false, 
+      twostoryhouse: true, 
+      apartment: false, 
+      townhouse: false
+    }
+  };
 }
