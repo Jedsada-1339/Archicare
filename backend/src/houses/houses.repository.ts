52cdfa7d0aa1632760ce0a70ac8a43e5ea5578cc
Jedsada-1339 <jedsada.si@kkumail.com>
2025-06-13 16:22:22 +0700

    delete unused file

--- backend/src/houses/houses.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Entity, EntityRepository, Repository } from "typeorm";
-import { Houses } from "./houses.entity";
-
-@EntityRepository(Houses)
-export class HousesRepository extends Repository<Houses>{
-
-}
